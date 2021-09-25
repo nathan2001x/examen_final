aabeb9b02a49c742b43c24c1e9db4e55691e59de
Nathan Velez <veleznathan64@gmail.com>
2021-09-24 20:11:35 -0500

    commit 2

--- examen/examem/finals/Autos.java
+++ examen/examem/finals/Autos.java
@@ -5,35 +5,79 @@ public class Autos {
 	private String marca;
 	private String placa;
 	private String kilometraje;
+	private String anioFabricacion;
+	private String codigoNacional;
+	private String codigoDeImportacion;
+	private String valorDeImportacion;
+
 	public String getModelo() {
 		return modelo;
 	}
+
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
+
 	public String getMarca() {
 		return marca;
 	}
+
 	public void setMarca(String marca) {
 		this.marca = marca;
 	}
+
 	public String getPlaca() {
 		return placa;
 	}
+
 	public void setPlaca(String placa) {
 		this.placa = placa;
 	}
+
 	public String getKilometraje() {
 		return kilometraje;
 	}
+
 	public void setKilometraje(String kilometraje) {
 		this.kilometraje = kilometraje;
 	}
+
+	public String getAnioFabricacion() {
+		return anioFabricacion;
+	}
+
+	public void setAnioFabricacion(String anioFabricacion) {
+		this.anioFabricacion = anioFabricacion;
+	}
+
+	public String getCodigoNacional() {
+		return codigoNacional;
+	}
+
+	public void setCodigoNacional(String codigoNacional) {
+		this.codigoNacional = codigoNacional;
+	}
+
+	public String getCodigoDeImportacion() {
+		return codigoDeImportacion;
+	}
+
+	public void setCodigoDeImportacion(String codigoDeImportacion) {
+		this.codigoDeImportacion = codigoDeImportacion;
+	}
+
+	public String getValorDeImportacion() {
+		return valorDeImportacion;
+	}
+
+	public void setValorDeImportacion(String valorDeImportacion) {
+		this.valorDeImportacion = valorDeImportacion;
+	}
+
 	@Override
 	public String toString() {
-		return "Autos [marca=" + marca + ", modelo=" + modelo + ", placa=" + placa + ", kilometraje=" + kilometraje + "]";
+		return "Autos [marca=" + marca + ", modelo=" + modelo + ", placa=" + placa + ", kilometraje=" + kilometraje
+				+ "]";
 	}
-	
-	
 
 }
--- examen/examem/finals/Main.java
+++ examen/examem/finals/Main.java
@@ -8,6 +8,11 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner teclado=new Scanner(System.in);
 		int opcion;
+		String marcaIngresada,modeloIngresado,anioIngresado,placaIngresada,kilometrajeIngresad;
+		String kilometrajeIngresado,codigoNacional,codigoImportacionIngre,valorImportacionIngre,kilometrajeNuevo;
+		Autos vector[]=new Autos[4];
+		int i=-1;
+		
 		do {
 		System.out.println("########################################");
 		System.out.println("CONCESIONARIO AMBACAR, elija una opción:");
@@ -26,26 +31,89 @@ public class Main {
 			int opcionTipoAuto=teclado.nextInt();
 			if(opcionTipoAuto==1) {
 				System.out.println("Ingrese marca del auto");
+				marcaIngresada=teclado.nextLine();
+				Autos marcaAuto=new Autos();
+				marcaAuto.setMarca(marcaIngresada);
+				
 				System.out.println("Ingrese modelo del auto");
+				modeloIngresado=teclado.nextLine();
+				Autos modeloAuto=new Autos();
+				modeloAuto.setModelo(modeloIngresado);
+				
 				System.out.println("Ingrese año de fabricacion del auto");
+				anioIngresado=teclado.nextLine();
+				Autos anioAuto=new Autos();
+				anioAuto.setAnioFabricacion(anioIngresado);
+				
 				System.out.println("Ingrese placa del auto");
+				placaIngresada=teclado.nextLine();
+				Autos placaAuto=new Autos();
+				placaAuto.setPlaca(placaIngresada);
+				
 				System.out.println("Ingrese kilometraje del auto");
+				kilometrajeIngresado=teclado.nextLine();
+				Autos kilometrajeAuto=new Autos();
+				kilometrajeAuto.setKilometraje(kilometrajeIngresado);
+				
 				System.out.println("Ingrese codigo nacional del auto");
+				codigoNacional=teclado.nextLine();
+				Autos auto1=new Autos();
+				auto1.setCodigoNacional(codigoNacional);
+				i=i+1;
+				vector[]=;
+				
 			} else if(opcionTipoAuto==2);
 			System.out.println("Ingrese marca del auto");
+			marcaIngresada=teclado.nextLine();
+			Autos marcaAuto=new Autos();
+			marcaAuto.setMarca(marcaIngresada);
+			
 			System.out.println("Ingrese modelo del auto");
+			modeloIngresado=teclado.nextLine();
+			Autos modeloAuto=new Autos();
+			modeloAuto.setModelo(modeloIngresado);
+			
 			System.out.println("Ingrese año de fabricacion del auto");
+			anioIngresado=teclado.nextLine();
+			Autos anioAuto=new Autos();
+			anioAuto.setAnioFabricacion(anioIngresado);
+			
 			System.out.println("Ingrese placa del auto");
+			placaIngresada=teclado.nextLine();
+			Autos placaAuto=new Autos();
+			placaAuto.setPlaca(placaIngresada);
+			
 			System.out.println("Ingrese kilometraje del auto");
+			kilometrajeIngresado=teclado.nextLine();
+			Autos kilometrajeAuto=new Autos();
+			kilometrajeAuto.setKilometraje(kilometrajeIngresado);
+			
 			System.out.println("Ingrese codigo de importacion");
+			codigoImportacionIngre=teclado.nextLine();
+			Autos codigoImportacion=new Autos();
+			codigoImportacion.setCodigoDeImportacion(codigoImportacionIngre);
+			
 			System.out.println("Ingrese valor de importacion");
+			valorImportacionIngre=teclado.nextLine();
+			Autos valorImportacion=new Autos();
+			valorImportacion.setValorDeImportacion(valorImportacionIngre);
+			
 			break;
 		case 2:
 			System.out.println("Ingrese placa del auto");
+			placaIngresada=teclado.nextLine();
+			System.out.println("Ingrese nuevo kilometraje");
+			kilometrajeNuevo=teclado.nextLine();
 			break;
 		case 3:
 			System.out.println("Ingrese numero de placa");
 			break;
+		case 4:
+			break;
+			default:
+				System.out.println("Escoge una opcion valida porfavor");
+				break;
+				
 		}
 		
 		
